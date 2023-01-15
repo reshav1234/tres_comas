67e9fd322934e9bef52cf6c31534bfe499997b22
Suyog04 <72078239+Suyog04@users.noreply.github.com>
2023-01-15 18:35:04 +0545

    cc

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -24,7 +24,6 @@ const Home = () => {
                 </ul>
                 
             </div>
-
             <div className = "search-bar">
                 <h1>Location</h1>
                 <form>
