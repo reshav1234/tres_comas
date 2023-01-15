5a1926ff400a22cc25551a90261c3ae710931fa6
Suyog04 <72078239+Suyog04@users.noreply.github.com>
2023-01-15 16:04:06 +0545

    cc

---
+++ src/Pages/AboutUs/Aboutus.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Aboutus = () => {
+  return (
+    <h2>We are a e-commerce website ehich helps in buying lands,plots in a easier and convinient way.</h2>
+  )
+}
+
+export default Aboutus
